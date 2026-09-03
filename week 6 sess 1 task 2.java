0969aad9fc66ea1938d201dcaa9831d3bfb1d069
vtu30386-sudo <vtu30386@veltech.edu.in>
2026-09-03 09:44:17 +0530

    Add files via upload

---
+++ week 6 sess 1 task 2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+
+        String doubled = s + s;
+
+        return doubled.substring(1, doubled.length() - 1).contains(s);
+    }
+}
+
+output:
+nput
+s =
+"abab"
+Output
+true
+Expected
+true
+
